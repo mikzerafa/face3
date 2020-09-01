16d198e1a8de2a93e55964a17f1750e73e6cef0d
mickey <mikzerafa@hotmail.com>
2020-09-01 22:10:33 +0200

    added eye and face detection

---
+++ main.py
@@ -0,0 +1,31 @@
+import cv2
+import sys
+
+
+cascPath = "/home/mickey/PycharmProjects/face3/venv/lib/python3.6/site-packages/cv2/data/haarcascade_frontalface_default.xml"
+eyecascPath = "/home/mickey/PycharmProjects/face3/venv/lib/python3.6/site-packages/cv2/data/haarcascade_eye.xml"
+
+faceCascade = cv2.CascadeClassifier(cascPath)
+eyeCascade = cv2.CascadeClassifier(eyecascPath)
+
+video_capture = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = video_capture.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(gray, 1.1, 4)
+    eyes = eyeCascade.detectMultiScale(gray, 1.1, 4)
+
+    for (x, y, w, h) in eyes:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    for(x,y,w,h) in faces:
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 2)
+
+    cv2.imshow('Video', frame)
+
+    if(cv2.waitKey(1) & 0xFF == ord('q')):
+        break
+
+video_capture.release()
+cv2.destroyAllWindows()
